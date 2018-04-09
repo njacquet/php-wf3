5bb8c64cb38aece20327e18d79579c487a2641d0
njacquet <natalya.sparrow@gmail.com>
2018-04-09 14:52:40 +0200

    contenu accueil

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once __DIR__ . '/include/init.php';
 include __DIR__ . '/layouts/top.php';
  ?>
  <h1>Page d'accueil</h1>
+ <p>Bonjour</p>
  <?php 
 include __DIR__ . '/layouts/bottom.php';
 
